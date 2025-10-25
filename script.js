2951cdd5b7a0bb9bb52ddce093e01e23828cff64
Abhijeet1234567890 <gajadhanetathaget@gmail.com>
2025-10-25 23:00:02 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Typing Effect
+const text = ["Full Stack Web Developer", "React & Node.js Enthusiast", "Creative Problem Solver"];
+let index = 0;
+let charIndex = 0;
+const typingElement = document.querySelector(".typing");
+
+function type() {
+  if (charIndex < text[index].length) {
+    typingElement.textContent += text[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingElement.textContent = text[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    index = (index + 1) % text.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+});
